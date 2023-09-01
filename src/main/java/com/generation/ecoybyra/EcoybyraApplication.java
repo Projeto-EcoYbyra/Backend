4e628d5eb819410ff4658469b9b1f1f299de7413
unknown <guilherme.neto2001@outlook.com>
2023-09-01 14:19:14 -0300

    Criar projeto spring

---
+++ src/main/java/com/generation/ecoybyra/EcoybyraApplication.java
@@ -0,0 +1,13 @@
+package com.generation.ecoybyra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoybyraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoybyraApplication.class, args);
+	}
+
+}
