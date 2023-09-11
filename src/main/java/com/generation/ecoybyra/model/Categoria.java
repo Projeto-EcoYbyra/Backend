6c918d643b6ea3c186a7271cd11900327b477c53
pedrohenrique096 <moreiraspedro96@gmail.com>
2023-09-11 10:23:40 -0300

    Relação entre as classes Model

--- src/main/java/com/generation/ecoybyra/model/Categoria.java
+++ src/main/java/com/generation/ecoybyra/model/Categoria.java
@@ -1,12 +1,11 @@
 package com.generation.ecoybyra.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_categorias")
@@ -23,6 +22,18 @@ public class Categoria {
 	@NotBlank(message = "O atributo descrição é obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo descrição deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String descricao;
+
+	@OneToMany (fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties ("categoria")
+	private List <Produto> produto;
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public Long getId() {
 		return id;
--- src/main/java/com/generation/ecoybyra/model/Produto.java
+++ src/main/java/com/generation/ecoybyra/model/Produto.java
@@ -2,12 +2,8 @@ package com.generation.ecoybyra.model;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -34,6 +30,30 @@ public class Produto {
 
 	@NotBlank(message = "O atributo foto é Obrigatório!")
 	private String foto;
+
+	@ManyToOne
+	@JsonIgnoreProperties ("produto")
+	private Categoria categoria;
+
+	@ManyToOne
+	@JsonIgnoreProperties ("produto")
+	private Usuario usuario;
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public Long getId() {
 		return id;
