13a34cfb1a4e1ba70de22656692d9397532ae6ff
unknown <kvrah.eo.2013@gmail.com>
2023-09-20 11:44:45 -0300

    versao deploy

--- src/main/java/com/generation/ecoybyra/model/Usuario.java
+++ src/main/java/com/generation/ecoybyra/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -27,6 +28,7 @@ public class Usuario {
 		@NotBlank(message = "O atributo nome é Obrigatório!")
 		private String nome;
 
+		@Schema(example = "email@email.com.br")
 		@NotBlank(message = "O atributo usuário é Obrigatório!")
 		@Email(message = "O Atributo Usuário deve ser um email válido!")
 		private String usuario;
--- src/main/java/com/generation/ecoybyra/service/UsuarioService.java
+++ src/main/java/com/generation/ecoybyra/service/UsuarioService.java
@@ -59,29 +59,27 @@ public class UsuarioService {
 
     public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
 
-        // Gera o Objeto de autenticação
         var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
 
-        // Autentica o Usuario
+
         Authentication authentication = authenticationManager.authenticate(credenciais);
 
-        // Se a autenticação foi efetuada com sucesso
+
         if (authentication.isAuthenticated()) {
 
-            // Busca os dados do usuário
+
             Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
 
-            // Se o usuário foi encontrado
+
             if (usuario.isPresent()) {
 
-                // Preenche o Objeto usuarioLogin com os dados encontrados
+
                 usuarioLogin.get().setId(usuario.get().getId());
                 usuarioLogin.get().setNome(usuario.get().getNome());
                 usuarioLogin.get().setFoto(usuario.get().getFoto());
                 usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
                 usuarioLogin.get().setSenha("");
 
-                // Retorna o Objeto preenchido
                 return usuarioLogin;
 
             }
